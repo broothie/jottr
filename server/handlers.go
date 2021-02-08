c6335e2759b95cadc1263b0d13e3c8118e6865a4
Andrew Booth <adbooth8@gmail.com>
2021-02-08 18:23:06 +0000

    set cache control headers

--- server/handlers.go
+++ server/handlers.go
@@ -16,55 +16,52 @@ type Jot struct {
 	Body string `json:"body" firestore:"body"`
 }
 
-func (s *Server) newJot() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		id := newID()
-		if _, err := s.db.Collection("jots").Doc(id).Set(r.Context(), Jot{}); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		http.Redirect(w, r, fmt.Sprintf("/jot/%s", id), http.StatusPermanentRedirect)
+func (s *Server) newJot(w http.ResponseWriter, r *http.Request) {
+	id := newID()
+	if _, err := s.db.Collection("jots").Doc(id).Set(r.Context(), Jot{}); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
+	http.Redirect(w, r, fmt.Sprintf("/jot/%s", id), http.StatusPermanentRedirect)
 }
 
-func (s *Server) showJot() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		id := httprouter.ParamsFromContext(r.Context()).ByName("id")
-		doc, err := s.db.Collection("jots").Doc(id).Get(r.Context())
-		if err != nil {
-			if status.Code(err) == codes.NotFound {
-				s.render.HTML(w, http.StatusNotFound, "jots/404", nil)
-				return
-			}
-
-			s.render.HTML(w, http.StatusInternalServerError, "error", nil)
+func (s *Server) showJot(w http.ResponseWriter, r *http.Request) {
+	id := httprouter.ParamsFromContext(r.Context()).ByName("id")
+	doc, err := s.db.Collection("jots").Doc(id).Get(r.Context())
+	if err != nil {
+		if status.Code(err) == codes.NotFound {
+			s.render.HTML(w, http.StatusNotFound, "jots/404", nil)
 			return
 		}
 
-		s.render.HTML(w, http.StatusOK, "jots/show", map[string]interface{}{
-			"id":            id,
-			"body":          template.HTML(doc.Data()["body"].(string)),
-			"save_delay_ms": s.config.SaveDelayMs,
-		})
+		s.render.HTML(w, http.StatusInternalServerError, "error", nil)
+		return
 	}
+
+	s.render.HTML(w, http.StatusOK, "jots/show", map[string]interface{}{
+		"id":            id,
+		"body":          template.HTML(doc.Data()["body"].(string)),
+		"save_delay_ms": s.config.SaveDelayMs,
+	})
 }
 
-func (s *Server) syncJot() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		id := httprouter.ParamsFromContext(r.Context()).ByName("id")
+func (s *Server) syncJot(w http.ResponseWriter, r *http.Request) {
+	id := httprouter.ParamsFromContext(r.Context()).ByName("id")
 
-		var jot Jot
-		if err := json.NewDecoder(bufio.NewReader(r.Body)).Decode(&jot); err != nil {
-			s.render.JSON(w, http.StatusBadRequest, map[string]string{"message": err.Error()})
-			return
-		}
-
-		if _, err := s.db.Collection("jots").Doc(id).Set(r.Context(), jot); err != nil {
-			s.render.JSON(w, http.StatusInternalServerError, map[string]string{"message": err.Error()})
-			return
-		}
-
-		s.render.JSON(w, http.StatusOK, map[string]string{"body": ""})
+	var jot Jot
+	if err := json.NewDecoder(bufio.NewReader(r.Body)).Decode(&jot); err != nil {
+		s.render.JSON(w, http.StatusBadRequest, map[string]string{"message": err.Error()})
+		return
 	}
+
+	if _, err := s.db.Collection("jots").Doc(id).Set(r.Context(), jot); err != nil {
+		s.render.JSON(w, http.StatusInternalServerError, map[string]string{"message": err.Error()})
+		return
+	}
+
+	s.render.JSON(w, http.StatusOK, map[string]string{"body": ""})
 }
--- server/routes.go
+++ server/routes.go
@@ -9,9 +9,9 @@ import (
 func (s *Server) routes() http.Handler {
 	router := httprouter.New()
 
-	router.NotFound = s.newJot()
-	router.HandlerFunc(http.MethodGet, "/jot/:id", s.showJot())
-	router.HandlerFunc(http.MethodPut, "/jot/:id/sync", s.syncJot())
+	router.NotFound = http.HandlerFunc(s.newJot)
+	router.HandlerFunc(http.MethodGet, "/jot/:id", s.showJot)
+	router.HandlerFunc(http.MethodPut, "/jot/:id/sync", s.syncJot)
 
 	return router
 }
